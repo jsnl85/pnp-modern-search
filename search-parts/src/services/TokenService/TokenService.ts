5125d98d3cf7ce08f9bfa4d33c29de7cadf5f083
Joao Lopes <joaol@superfluid.com.au>
2020-10-29 12:16:41 +1100

    *support to filter using KQL GPP/GPSet/GP0 criteria *pre-packaged files

--- search-parts/src/services/TokenService/TokenService.ts
+++ search-parts/src/services/TokenService/TokenService.ts
@@ -7,6 +7,9 @@ import { isEmpty } from '@microsoft/sp-lodash-subset';
 
 const LOG_SOURCE: string = '[SearchResultsWebPart_{0}]';
 
+const _rxGuid = /.*([0-9a-f]{8})-?([0-9a-f]{4})-?([0-9a-f]{4})-?([0-9a-f]{4})-?([0-9a-f]{12}).*/g;
+function parseGuid(termId: string): string { let ret = (''+(termId||'')).replace(_rxGuid,"$1-$2-$3-$4-$5"); return ret.toLowerCase(); }
+
 export class TokenService implements ITokenService {
     private _pageContext: PageContext;
     private _spHttpClient: SPHttpClient;
@@ -23,6 +26,8 @@ export class TokenService implements ITokenService {
         queryTemplate = this.replaceHubSiteTokens(queryTemplate);
         queryTemplate = this.replaceOrOperator(queryTemplate);
         queryTemplate = this.replaceUrlTokens(queryTemplate);
+        queryTemplate = this.replaceTaxGPPL0(queryTemplate);
+        queryTemplate = this.replaceEmptyConditions(queryTemplate);
 
         queryTemplate = queryTemplate.replace("{TenantUrl}", `https://` + window.location.host);
 
@@ -125,11 +130,52 @@ export class TokenService implements ITokenService {
         let reQueryTemplate = queryTemplate;
         let match = queryStringVariables.exec(reQueryTemplate);
         if (match != null) {
-            var queryParameters = new UrlQueryParameterCollection(window.location.href);
+            let queryParameters = new UrlQueryParameterCollection(window.location.href);
+            let hashParameters = null; try { hashParameters = new UrlQueryParameterCollection(window.location.protocol+"//"+window.location.host+((window.location.port!=="80"&&window.location.port!=="443")?":"+window.location.port:'')+"/"+window.location.pathname+'?'+(window.location.hash||'').replace(/^\#/,'')); } catch(ex) { }
             while (match !== null) {
                 let qsProp = match[1];
-                let itemProp = decodeURIComponent(queryParameters.getValue(qsProp) || "");
+                let itemProp = decodeURIComponent(queryParameters.getValue(qsProp)||(hashParameters?hashParameters.getValue(qsProp):null)||"");
                 queryTemplate = queryTemplate.replace(match[0], itemProp);
+                match = queryStringVariables.exec(reQueryTemplate);
+            }
+        }
+        return queryTemplate;
+    }
+
+    private replaceTaxGPPL0(queryTemplate: string) {
+        const queryStringVariables = /\{\?([+-]?)Tax((?:GTSet|GPP|L0)*)?[_]?(.+)?(>=|=|<=|:|<>|<|>)(.+)\}/gi;
+        let reQueryTemplate = queryTemplate;
+        let match = queryStringVariables.exec(reQueryTemplate);
+        if (match != null) {
+            while (match !== null) {
+                // sample value: GP0|#852f6120-50aa-4565-ac57-345a649bbf2e;L0|#0852f6120-50aa-4565-ac57-345a649bbf2e|Civil Engineering, n.e.c.;GTSet|#bb4fb6d6-fa7c-4e19-9d64-dd26ee30e7a0;GPP|#b9071a78-a083-4e61-8b15-94dc78494fe9;GPP|#0305f571-260e-4746-968c-7218cf4fc6d8
+                // GP0: only Specific TermId
+                // GPP: only Children Of TermId
+                let modifier = match[1]; let taxMode = match[2]; let propertyName = match[3]||'owstaxidmetadataalltagsinfo'; let condition = match[4]; let id = parseGuid(match[5]);
+                if (id) {
+                    let taxTermSetOnlyQ  = (!taxMode || /GTSet/gi.test(taxMode));
+                    let taxChildrenOnlyQ = (!taxMode || /GPP/gi.test(taxMode));
+                    let taxSpecificOnlyQ = (!taxMode || /L0/gi.test(taxMode)); if (taxSpecificOnlyQ) { taxTermSetOnlyQ = false; }
+                    let parts = [];
+                    if (taxTermSetOnlyQ)  { parts.push(`${modifier}${propertyName}${condition}GTSet|#${id}`); }
+                    if (taxChildrenOnlyQ) { parts.push(`${modifier}${propertyName}${condition}#${id}`); }
+                    if (taxSpecificOnlyQ) { parts.push(`${modifier}${propertyName}${condition}#0${id}`); }
+                    let replacementQuery = ((parts.length>1)?`(${parts.join(' OR ')})`:parts.join(' OR '));
+                    queryTemplate = queryTemplate.replace(match[0], replacementQuery);
+                    match = queryStringVariables.exec(reQueryTemplate);
+                }
+            }
+        }
+        return queryTemplate;
+    }
+
+    private replaceEmptyConditions(queryTemplate: string) {
+        const queryStringVariables = /\{\?(?:[+-]?).+(?:>=|=|<=|:|<>|<|>)\}/gi;
+        let reQueryTemplate = queryTemplate;
+        let match = queryStringVariables.exec(reQueryTemplate);
+        if (match != null) {
+            while (match !== null) {
+                queryTemplate = queryTemplate.replace(match[0], "");
                 match = queryStringVariables.exec(reQueryTemplate);
             }
         }
